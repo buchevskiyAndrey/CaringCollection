5269a027ebd92cd0d6cde31aed6a79e9c8a40835
Andrey Buchevskiy <aabuchevsk@mts.ru>
2024-04-26 13:36:48 +0300

    Initial Commit

---
+++ CaringCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaringCollection
+//
+//  Created by Бучевский Андрей on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
