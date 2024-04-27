8705d6588b9e782298d8af58b1ed53dc9481b924
Andrey Buchevskiy <buchevskiy@icloud.com>
2024-04-27 13:31:29 +0300

    Caring Collection

--- CaringCollection/ViewController.swift
+++ CaringCollection/ViewController.swift
@@ -9,11 +9,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let flowLayout = FlowLayout()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.description())
+
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+
+        title = "Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: collectionView.bounds.size.width * 0.8,
+            height: collectionView.bounds.size.height * 0.8
+        )
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        .init(top: 0, left: collectionView.layoutMargins.left, bottom: 0, right: collectionView.layoutMargins.right)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UICollectionViewCell.description(), for: indexPath)
+        cell.backgroundColor = .systemGray5
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
+
+final class FlowLayout: UICollectionViewFlowLayout {
+
+    override func prepare() {
+        super.prepare()
+        scrollDirection = .horizontal
+        minimumLineSpacing = 10
+    }
+
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint,
+                                      withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        let index = getIndex(offset: proposedContentOffset)
+        let projectedOffset = contentOffset(for: index)
+        return projectedOffset
+    }
+
+    private func getIndex(offset: CGPoint) -> Int {
+        let itemWidth = collectionView!.bounds.size.width * 0.8 + 10
+        let index = (offset.x - collectionView!.layoutMargins.left) / itemWidth
+
+        return Int(index)
+    }
+
+    private func contentOffset(for index: Int) -> CGPoint {
+        let itemWidth = collectionView!.bounds.size.width * 0.8 + 10
+
+        return CGPoint(x: itemWidth * CGFloat(index), y: collectionView!.contentOffset.y)
+    }
+}
